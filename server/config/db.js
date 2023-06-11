f22cd2e4d5eeab592b437e46dbd213eaced24e42
Rohan Magar <rohan.magar.415@gmail.com>
2023-06-11 22:53:35 +0545

    setting up the migration

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("expenseTracker", "root", "1234", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+  }
+);
 
 module.exports = sequelize;
